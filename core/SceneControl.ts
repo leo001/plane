69dc41c11a9ff7829f67b9d71702c8358579d8c7
leo001 <leotz882@163.com>
2016-10-14 11:46:47 +0800

    Create SceneControl.ts

---
+++ core/SceneControl.ts
@@ -0,0 +1,22 @@
+class SceneControl{
+	protected _scene:BaseScene;
+
+	public constructor() {
+	}
+
+	public createScene(data?:any):void{
+
+	}
+
+	public init(data?:any):void{
+
+	}
+
+	public dispose():void{
+
+	}
+
+	public get scene():BaseScene{
+		return this._scene;
+	}
+}
