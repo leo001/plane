b3a267023cc5e4ed959f21c62abff0c2e58b2d75
leo001 <leotz882@163.com>
2016-10-14 11:34:06 +0800

    Update PanelName.ts

--- panel/PanelName.ts
+++ panel/PanelName.ts
@@ -1,7 +1,37 @@
 class PanelName {
-	public static HELP:string = 'HELP';
-	//other panel
-	public static RESULT_1:string = 'RESULT_1';
-	public static RESULT_2:string = 'RESULT_2';
-	public static RESULT_3:string = 'RESULT_3';
+
+	//fight
+	public static FIGHT_FAIL: string = 'FIGHT_FAIL';
+	public static Fight_WIN: string = 'Fight_WIN';
+
+
+	//hero info
+	public static HERO_VIEW: string = 'HERO_VIEW';
+	public static Embattle_VIEW: string = 'Embattle_VIEW';
+	public static HeroChoose_VIEW: string = 'HeroChoose_VIEW';
+	public static AlertPanel:string = 'AlertPanel';
+	//login
+	public static REG: string = 'REG';
+
+}
+
+class PanelClsMap {
+	public data: any;
+	public constructor() {
+		this.data = {};
+		this.data[PanelName.FIGHT_FAIL] = FightFailPanel;
+		this.data[PanelName.Fight_WIN] = FightWinPanel;
+
+
+		this.data[PanelName.HERO_VIEW] = HeroViewPanel;
+		this.data[PanelName.Embattle_VIEW] = EmbattlePanel;
+		this.data[PanelName.HeroChoose_VIEW] = HeroChoosePanel;
+		this.data[PanelName.AlertPanel] = AlertPanel;
+
+
+		this.data[PanelName.REG] = RegPanel;
+		
+
+
+	}
 }
